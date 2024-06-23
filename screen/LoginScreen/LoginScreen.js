bf37d4ee40e315b21ac58113e152f742789efb7a
Muhammad Arif Aditya <arifaditya1308@gmail.com>
2024-06-23 17:19:03 +0700

    FE-Login: creating login button

--- screen/LoginScreen/LoginScreen.js
+++ screen/LoginScreen/LoginScreen.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Button } from 'react-native';
 
 const LoginScreen = () => {
   return (
     <View>
       <Text>LoginScreen</Text>
+      <Button title="Login" />
     </View>
   );
 }
